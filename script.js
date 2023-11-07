17fca6aa179aace54206de49da4bf06cc3f2824d
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-07 19:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", () => {
-	document.body.innerText="DOM load success";
+	// document.body.innerText="DOM load success";
+	document.write("DOM load success")
 })
